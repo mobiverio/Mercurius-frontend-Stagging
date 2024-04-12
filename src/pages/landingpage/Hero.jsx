1508023d8d97b130c4d11366e0f07f0bca080378
Muhammad Najeeb <yusnaj21@gmail.com>
2024-04-12 22:53:04 +0100

    Modified:Slider component

--- src/pages/landingpage/Hero.jsx
+++ src/pages/landingpage/Hero.jsx
@@ -38,7 +38,7 @@ export const Slider = function ({
     return () => {
       clearInterval(slideInterval);
     };
-    //eslint_disable-next-line react-hook-exhaustive-dept
+    //eslint_disable-next-line react-hooks/exhaustive-deps
   }, [currentIndex]);
 
   return (
