20e08e467b3a217393750da69da6fab76b498042
Muhammad Najeeb <yusnaj21@gmail.com>
2024-06-19 00:09:57 +0100

    Modified: Missing page

--- src/components/molecules/404.js
+++ src/components/molecules/404.js
@@ -1,14 +1,17 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Missing = () => {
   return (
-    <div>
-      <h1>Ooops!</h1>
-      <p>
-        The page you are trying to access does not exits or has been moved
-        temporarily
+    <div className="flex flex-col justify-center items-center h-svh">
+      <h1 className="mercurius">Oops!</h1>
+      <p className="text-xl w-1/2 text-center my-4">
+        The page you are looking for might have been removed, has its name
+        changed or temporarily unavailable.
       </p>
-      <button>Home</button>
+      <button className="bg-black px-4 py-2 text-white hover:bg-black/80 rounded-lg">
+        <Link to="/">Home</Link>
+      </button>
     </div>
   );
 };
