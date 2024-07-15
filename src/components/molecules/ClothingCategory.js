15dbcb11c623ee81791686029a2ec9e8f44b0a6f
Muhammad Najeeb <yusnaj21@gmail.com>
2024-07-15 20:54:14 +0100

    Modified: Implemented Sticky Header

--- src/components/molecules/ClothingCategory.js
+++ src/components/molecules/ClothingCategory.js
@@ -7,7 +7,10 @@ const ClothingCategory = ({ category, items }) => {
       <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-5 gap-4">
         {items?.map((item) => {
           return (
-            <div className="text-center flex flex-col items-center justify-center">
+            <div
+              key={item?.id}
+              className="text-center flex flex-col items-center justify-center"
+            >
               <figure className="w-1/2 sm:w-[180px] sm:h-[180px] md:bg-gray-100 rounded-full flex flex-col justify-center items-center">
                 <img className="" src={item.src} alt="/" />
               </figure>
