1ec3d0d877dc16383eeab55cb8edadeffba2e590
Muhammad Najeeb <yusnaj21@gmail.com>
2024-07-17 15:05:45 +0100

    Modified: Product Accordion

--- src/pages/product-listing/ProductDescription.js
+++ src/pages/product-listing/ProductDescription.js
@@ -1,7 +1,6 @@
 const ProductDescription = ({ product }) => {
   return (
     <main className="w-full">
-      <h2 className="text-xl font-semibold">Description</h2>
       <p>{product?.description}</p>
     </main>
   );
